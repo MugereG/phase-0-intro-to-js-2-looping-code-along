bc5772cfd7faecd44849becad3398c07d3edc89d
Gideon Mugere <gideonnemugere@gmail.com>
2023-07-16 15:17:29 +0300

    Almost completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = [];
+  
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for attending the wonderful ${event} event.`;
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  
