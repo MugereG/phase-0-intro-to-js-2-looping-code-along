214fa6b8ea1e1adda8cc5d8eef5c8b6d44056335
Gideon Mugere <gideonnemugere@gmail.com>
2023-07-16 21:03:04 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // Code your solutions in this file
 function writeCards(names, event) {
-    const messages = [];
-  
-    for (let i = 0; i < names.length; i++) {
-      const message = `Thank you, ${names[i]}, for attending the wonderful ${event} event.`;
-      messages.push(message);
+    return names.map(name => `Thank you, ${name}, for the wonderful ${event} gift!`)
+  }
+
+  function countDown(number) {
+    for (let i = number; i >=0; i--){
+      console.log(i);
     }
-  
-    return messages;
   }
   
